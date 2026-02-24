b60769cf203d9213739a6749774895590ad7bd0c
Neiland85 <theia3impact@gmail.com>
2026-02-24 02:57:16 +0100

    fix(test): ensure vitest config consistent in CI

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vitest/config"
+import tsconfigPaths from "vite-tsconfig-paths"
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    environment: "node",
+    globals: true,
+    include: ["src/**/*.test.ts"],
+    reporters: ["default"],
+    passWithNoTests: false,
+    isolate: true
+  }
+})
